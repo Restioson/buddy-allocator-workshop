2722ecceea5538b45573873f585a6488addc7798
restioson <restiosondev@gmail.com>
2018-05-01 13:22:03 +0200

    Idek

--- benches/bitmap.rs
+++ benches/bitmap.rs
@@ -9,16 +9,15 @@ fn bitmap(c: &mut Criterion) {
     use buddy_allocator_workshop::buddy_allocator_bitmap::*;
     use buddy_allocator_workshop::{MAX_ORDER, MIN_ORDER};
 
-    let mut trees: [Tree; 64] = array_init::array_init(|_| Tree::new());
-    let mut current_tree = 0;
+    let mut tree = Tree::new();
 
     c.bench_function("bitmap allocate_exact", move |b| {
         b.iter(|| {
-            match trees[current_tree].alloc_exact(0) {
+            match tree.alloc_exact(0) {
                 Some(_) => (),
                 None => {
-                    current_tree += 1;
-                    trees[current_tree].alloc_exact(0);
+                    tree = Tree::new();
+                    tree.alloc_exact(0);
                 },
             };
         });
